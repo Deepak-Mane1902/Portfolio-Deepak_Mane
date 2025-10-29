4dac2da51cb398b59be376c7dca5557f8738c9c9
Deepak_Mane <manedeepak1902@gmail.com>
2025-10-29 08:52:10 +0530

    Implemented update

--- src/components/Myself/Myself.jsx
+++ src/components/Myself/Myself.jsx
@@ -61,7 +61,7 @@ const Myself = () => {
       </div>
 
       {/* About Me Intro */}
-      <div className="w-[90vw] ml-[-4vw] md:ml-[-6vw] h-full md:h-[90vh] flex flex-col-reverse md:flex-row">
+      <div className="w-[90vw] ml-[-4vw] md:ml-[-6vw] h-full md:h-[96vh] flex flex-col-reverse md:flex-row">
         <div
           ref={leftSideRef}
           className="left-side w-full md:pl-20 lg:px-18 pt-1 md:pt-8 h-1/2 md:w-[76vw] lg:w-1/2 md:h-full  flex flex-col items-center md:items-start gap-5"
@@ -87,16 +87,16 @@ const Myself = () => {
           <p className="text-xl md:text-2xl px-5 text-white/80">
             My goal is to grow as a developer and build web experiences that make a real impact.
           </p>
-          <button className="relative left-[-24vw] md:left-0 mx-5 px-8 py-4 bg-green-800 hover:bg-green-500 cursor-pointer transition-all ease-in-out duration-300 rounded-2xl text-white/90">
+          {/* <button className="relative left-[-24vw] md:left-0 mx-5 px-8 py-4 bg-green-800 hover:bg-green-500 cursor-pointer transition-all ease-in-out duration-300 rounded-2xl text-white/90"> */}
             <a
               href="https://api.whatsapp.com/send/?phone=+91-9960633200&text&type=phone_number&app_absent=0"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-xl flex items-center text-center gap-2"
+              className="relative left-[-24vw] md:left-0 mx-5 px-8 py-4 bg-green-800 hover:bg-green-500 cursor-pointer transition-all ease-in-out duration-300 rounded-2xl text-white/90 flex items-center gap-2"
             >
               Connect <FaWhatsapp />
             </a>
-          </button>
+          {/* </button> */}
         </div>
 
         <div
